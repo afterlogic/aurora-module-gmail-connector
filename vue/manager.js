641cfbb8055c0dacabd5adca6d9cb15ec899c5cd
nadine-shi <nadinshi@gmail.com>
2021-06-18 13:27:02 +0300

    jslint related fixes

--- vue/manager.js
+++ vue/manager.js
@@ -4,20 +4,19 @@ import eventBus from 'src/event-bus'
 import modulesManager from 'src/modules-manager'
 
 export default {
-	name: 'GMailConnector',
-	init (appData) {
-		if (modulesManager.isModuleAvailable('MailWebclient')) {
-			eventBus.$on('MailWebclient::GetOauthConnectorsData', params => {
-				if (!_.isArray(params.oauthConnectorsData))
-				{
-					params.oauthConnectorsData = [];
-				}
-				params.oauthConnectorsData.push({
-					name: 'Gmail',
-					type: 'gmail',
-					iconUrl: 'static/styles/images/modules/GMailConnector/logo_gmail.png'
-				})
-			})
-		}
-	},
+  name: 'GMailConnector',
+  init (appData) {
+    if (modulesManager.isModuleAvailable('MailWebclient')) {
+      eventBus.$on('MailWebclient::GetOauthConnectorsData', params => {
+        if (!_.isArray(params.oauthConnectorsData)) {
+          params.oauthConnectorsData = []
+        }
+        params.oauthConnectorsData.push({
+          name: 'Gmail',
+          type: 'gmail',
+          iconUrl: 'static/styles/images/modules/GMailConnector/logo_gmail.png'
+        })
+      })
+    }
+  },
 }
