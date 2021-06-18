1d49e74c15749a588ab105301d89f1d3fb8269a3
nadine-shi <nadinshi@gmail.com>
2021-06-18 16:39:57 +0300

    requiredModules functionality

--- vue/manager.js
+++ vue/manager.js
@@ -1,22 +1,22 @@
 import _ from 'lodash'
 
 import eventBus from 'src/event-bus'
-import modulesManager from 'src/modules-manager'
 
 export default {
-  name: 'GMailConnector',
+  moduleName: 'GMailConnector',
+
+  requiredModules: ['MailWebclient'],
+
   init (appData) {
-    if (modulesManager.isModuleAvailable('MailWebclient')) {
-      eventBus.$on('MailWebclient::GetOauthConnectorsData', params => {
-        if (!_.isArray(params.oauthConnectorsData)) {
-          params.oauthConnectorsData = []
-        }
-        params.oauthConnectorsData.push({
-          name: 'Gmail',
-          type: 'gmail',
-          iconUrl: 'static/styles/images/modules/GMailConnector/logo_gmail.png'
-        })
+    eventBus.$on('MailWebclient::GetOauthConnectorsData', params => {
+      if (!_.isArray(params.oauthConnectorsData)) {
+        params.oauthConnectorsData = []
+      }
+      params.oauthConnectorsData.push({
+        name: 'Gmail',
+        type: 'gmail',
+        iconUrl: 'static/styles/images/modules/GMailConnector/logo_gmail.png'
       })
-    }
+    })
   },
 }
