0e89ede031c5b19a9e410f32d1a5265020e84b1d
nadine-shi <nadinshi@gmail.com>
2021-07-09 09:40:32 +0300

    Turn off subscription before turning it on (to avoid several subscription)

--- vue/manager.js
+++ vue/manager.js
@@ -2,21 +2,24 @@ import _ from 'lodash'
 
 import eventBus from 'src/event-bus'
 
+const _getOauthConnectorsData = params => {
+  if (!_.isArray(params.oauthConnectorsData)) {
+    params.oauthConnectorsData = []
+  }
+  params.oauthConnectorsData.push({
+    name: 'Gmail',
+    type: 'gmail',
+    iconUrl: 'static/styles/images/modules/GMailConnector/logo_gmail.png'
+  })
+}
+
 export default {
   moduleName: 'GMailConnector',
 
   requiredModules: ['MailWebclient'],
 
   initSubscriptions (appData) {
-    eventBus.$on('MailWebclient::GetOauthConnectorsData', params => {
-      if (!_.isArray(params.oauthConnectorsData)) {
-        params.oauthConnectorsData = []
-      }
-      params.oauthConnectorsData.push({
-        name: 'Gmail',
-        type: 'gmail',
-        iconUrl: 'static/styles/images/modules/GMailConnector/logo_gmail.png'
-      })
-    })
+    eventBus.$off('MailWebclient::GetOauthConnectorsData', _getOauthConnectorsData)
+    eventBus.$on('MailWebclient::GetOauthConnectorsData', _getOauthConnectorsData)
   },
 }
