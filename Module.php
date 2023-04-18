cd07f104191f008c937db712984444476b57181d
Vasil Sokolov <vasil@afterlogic.com>
2023-04-18 11:19:41 +0200

    Refactored module settings

--- Module.php
+++ Module.php
@@ -34,6 +34,15 @@ class Module extends \Aurora\System\Module\AbstractModule
         return parent::Decorator();
     }
 
+    /**
+     *
+     * @return Settings
+     */
+    protected function GetModuleSettings()
+    {
+        return $this->oModuleSettings;
+    }
+
     protected function issetScope($sScope)
     {
         return \in_array($sScope, \explode(' ', $this->getConfig('Scopes')));
